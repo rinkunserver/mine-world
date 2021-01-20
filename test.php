946bc76b9c22b4d6462b12ea84759ca1878fa69b
rinkunserver <76058210+rinkunserver@users.noreply.github.com>
2021-01-21 02:09:27 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'vendor/autoload.php';
 
 $id_token = filter_input(INPUT_POST, 'id_token');
-define('CLIENT_ID', '<用意したクライアントID>');
+define('CLIENT_ID', '536759493090-7q0p3uikn8ju2tb3uek6qmbvcko8vm47.apps.googleusercontent.com');
 
 $client = new Google_Client(['client_id' => CLIENT_ID]); 
 $payload = $client->verifyIdToken($id_token);
