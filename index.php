2e04c7c169cab570edae0de1c24297075508e751
rinkunserver <76058210+rinkunserver@users.noreply.github.com>
2021-01-21 02:10:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -37,18 +37,6 @@
     <!-- Header End -->
 
     <script src="js/header-7.js"></script>
-    
-    <?php
-session_start();
-if(!$_SESSION['login']){
-    header('Location: login.html');
-}
-?>
-<html>
-<body>
-    ログイン成功！    
-</body>
-</html>
-    
+   
   </body>
 </html>
