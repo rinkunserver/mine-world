66cab595cca44dbd38d9883246dd3ccbad743e83
rinkunserver <76058210+rinkunserver@users.noreply.github.com>
2021-01-20 20:52:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
-<h1>dsdfsds</h1>
-<h2>dsdfa</h2>
+<!doctype html>
+<html>
+<head>
+<title>マイクラサーバー</title>
+<style>
+<!--
+header {
+    display:block;
+    background:#666666;
+    width:1000px;
+    height:180px;
+}
+main {
+    display:block;
+    background:#aaaaaa;
+    width:1000px;
+    height:400px;
+}
+footer {
+    display:block;
+    background:#666666;
+    width:1000px;
+    height:50px;
+}
+-->
+</style>
+</head>
+<body>
+ 
+<header>マイクラサーバー</header>
+<main>メイン</main>
+<footer>コピーライト</footer>
+ 
+</body>
+</html>
 
-
-<h1>ここにペーストする</h1>
-
-
-<h2>ここに張り付けたり書いたりする</h2>
