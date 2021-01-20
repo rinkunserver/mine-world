590bbbed0237af60ff5bea259e872512ca5d506a
rinkunserver <76058210+rinkunserver@users.noreply.github.com>
2021-01-21 01:32:45 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,41 @@
-<!doctype html>
-<html>
-<head>
-<title>マイクラサーバー</title>
-<style>
-<!--
-header {
-    display:block;
-    background:#666666;
-    width:1000px;
-    height:180px;
-}
-main {
-    display:block;
-    background:#aaaaaa;
-    width:1000px;
-    height:400px;
-}
-footer {
-    display:block;
-    background:#666666;
-    width:1000px;
-    height:50px;
-}
--->
-</style>
-</head>
-<body>
- 
-<header>マイクラサーバー</header>
-<main>メイン</main>
-<footer>コピーライト</footer>
- 
-</body>
-</html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>マイクラ超非公式フォーラム</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="styles/reset.min.css" />
+    <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="styles/header-7.css" />
+  </head>
+  <body>
+    <!-- Header Start -->
+    <header class="site-header">
+      <div class="wrapper site-header__wrapper">
+        <div class="site-header__start">
+          <a href="#" class="brand">Brand</a>
+        </div>
+        <div class="site-header__middle">
+          <nav class="nav">
+            <button class="nav__toggle" aria-expanded="false" type="button">
+              menu
+            </button>
+            <ul class="nav__wrapper">
+              <li class="nav__item"><a href="#">ホーム</a></li>
+              <li class="nav__item"><a href="#">統合版</a></li>
+              <li class="nav__item"><a href="#">Java版</a></li>
+              <li class="nav__item"><a href="#">フリー</a></li>
+              <li class="nav__item"><a href="#">サイトの使い方</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="site-header__end">
+          <a href="#">サインイン</a>
+        </div>
+      </div>
+    </header>
+    <!-- Header End -->
 
+    <script src="js/header-7.js"></script>
+  </body>
+</html>
