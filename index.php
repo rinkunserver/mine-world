36ab2c3157a9adcb977269e3bd0ba0b4106825ce
rinkunserver <76058210+rinkunserver@users.noreply.github.com>
2021-01-21 01:41:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <title>マイクラ超非公式フォーラム</title>
+    <title>Headers - 7</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="styles/header.css" />
+    <link rel="stylesheet" href="styles/reset.min.css" />
+    <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="styles/header-7.css" />
   </head>
   <body>
     <!-- Header Start -->
@@ -19,21 +21,21 @@
               menu
             </button>
             <ul class="nav__wrapper">
-              <li class="nav__item"><a href="#">ホーム</a></li>
-              <li class="nav__item"><a href="#">統合版</a></li>
-              <li class="nav__item"><a href="#">Java版</a></li>
-              <li class="nav__item"><a href="#">フリー</a></li>
-              <li class="nav__item"><a href="#">サイトの使い方</a></li>
+              <li class="nav__item"><a href="#">Home</a></li>
+              <li class="nav__item"><a href="#">About</a></li>
+              <li class="nav__item"><a href="#">Services</a></li>
+              <li class="nav__item"><a href="#">Hire us</a></li>
+              <li class="nav__item"><a href="#">Contact</a></li>
             </ul>
           </nav>
         </div>
         <div class="site-header__end">
-          <a href="#">サインイン</a>
+          <a href="#">Sign in</a>
         </div>
       </div>
     </header>
     <!-- Header End -->
 
-    <script src="style/header.js"></script>
+    <script src="js/header-7.js"></script>
   </body>
 </html>
